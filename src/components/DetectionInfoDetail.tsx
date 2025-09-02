254e0e721aac853868b87650837f48c4aaa3791e
JINJIYU23 <dhkswksla22@naver.com>
2025-09-02 10:28:28 +0900

    First commit

---
+++ src/components/DetectionInfoDetail.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DetectionInfoDetail() {
+  return <div></div>;
+}
---
+++ src/components/LLMTextHistoryDetail.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LLMTextHistoryDetail() {
+  return <div></div>;
+}
