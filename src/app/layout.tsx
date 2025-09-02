e0a6422ab5d0b421f2b7d8a0ef913d80ae03ed48
JINJIYU23 <dhkswksla22@naver.com>
2025-09-02 14:15:13 +0900

    Style : 레이아웃

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import "@/styles/global.css";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html lang="ko">
       <body>{children}</body>
     </html>
   );
--- src/components/DetectionInfo.tsx
+++ src/components/DetectionInfo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function DetectionInfo() {
-  return <div></div>;
+  return (
+    <div className="ml-5 rounded-[10px] bg-[var(--box-color)] h-[600px]">
+      <h2 className="text-[24px] font-semibold text-center p-[15px]">
+        Detection Info
+      </h2>
+    </div>
+  );
 }
