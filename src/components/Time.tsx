b0babfe3ea7ad537a9d1a2f4f5c2a731e071f18b
JINJIYU23 <dhkswksla22@naver.com>
2025-09-03 15:49:24 +0900

    Feature:날씨 api 연결

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Time() {
-  return <div></div>;
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, "0");
+      const minutes = String(now.getMinutes()).padStart(2, "0");
+      const seconds = String(now.getSeconds()).padStart(2, "0");
+      setTime(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return <div className="text-[32px] font-semibold">{time}</div>;
 }
